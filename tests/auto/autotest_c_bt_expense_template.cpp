b399d9bbe87ef631d6a93c07ef2cd0768fb5072e
ikuchin <ikuchin@dev.connme.ru>
2018-11-27 07:56:20 -0500

    tests linking bugfix

--- tests/auto/autotest_c_bt_expense_template.cpp
+++ tests/auto/autotest_c_bt_expense_template.cpp
@@ -1,26 +1,83 @@
-// #include<stdio.h>
-#include<string>
+#include <string>
+#include <iostream>
+#include "ccgi.h"
+#include "cmysql.h"
+#include "cuser.h"
+#include "cstatistics.h"
+#include "utilities.h"
+#include "utilities_common.h"
+#include "localy.h"
 #include "c_bt.h"
 
 using namespace std;
 
 string Test1()
 {
-	string error_message = "";
+	auto error_message = ""s;
+	C_ExpenseTemplate	expense_template;
+
+	expense_template.SetID			("");
+	expense_template.SetTitle		("expense title");
+	expense_template.SetDescription	("expense description");
+
+	error_message = expense_template.CheckValidity(NULL);
 
 	return error_message;
 }
 
+string Test2()
+{
+	auto error_message = ""s;
+	C_ExpenseTemplate	expense_template;
+
+	expense_template.SetID			("0");
+	expense_template.SetTitle		("expense title");
+	expense_template.SetDescription	("expense description");
+
+	error_message = expense_template.CheckValidity(NULL);
+
+	return error_message;
+}
+
+
 int main(void)
 {
-	string	error_message = "";
+	CStatistics		appStat;  // --- CStatistics must be firts statement to measure end2end param's
+	CCgi			indexPage(EXTERNAL_TEMPLATE);
+	CUser			user;
+	CMysql			db;
+	auto			testing_success = true;
 
-	error_message = Test1();
+	{ MESSAGE_DEBUG("", "", __FILE__); }
+	signal(SIGSEGV, crash_handler);
 
-	if(error_message.empty())
-		printf("Test passed, but with failure");
-	else
-		printf("Test failed");
+/*
+	if(db.Connect(DB_NAME, DB_LOGIN, DB_PASSWORD) < 0)
+	{
+		MESSAGE_ERROR("", "", "Can not connect to mysql database");
+		throw CExceptionHTML("MySql connection");
+	}
+
+#ifndef MYSQL_3
+	db.Query("set names cp1251");
+#endif
+
+	indexPage.SetDB(&db);
+*/
+
+	if(Test1().length())
+	{
+		testing_success = false;
+		cout << "Test1 failed" << endl;
+	}
+	if(Test2().length())
+	{
+		testing_success = false;
+		cout << "Test2 failed" << endl;
+	}
+
+	if(testing_success)
+		cout << "Test passed" << endl;
 
 	return(0);
 }
