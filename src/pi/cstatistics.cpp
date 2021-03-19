1d332d76ddf8e4be75e6b6eb633de441ae9faee2
Ivan Kuchin <ivan.kuchin@gmail.com>
2021-03-18 22:28:10 -0400

    code refinement

--- src/pi/cstatistics.cpp
+++ src/pi/cstatistics.cpp
@@ -13,20 +13,7 @@ CStatistics::~CStatistics()
 	endTime = time(NULL);
 	endChrono = std::chrono::duration_cast<std::chrono::microseconds> (system_clock::now().time_since_epoch());
 
-	{
-		CLog			log(logFilename);
-		ostringstream	ost;
-
-		ost.str("");
-		ost	<< "CStatistics::~CStatistics: time duration (in ticks) = " << (float)(endClock - startClock) /  CLOCKS_PER_SEC;
-		log.Write(DEBUG, ost.str());
-
-		ost.str("");
-		ost	<< "CStatistics::~CStatistics: sec duration = " << (endTime - startTime);
-		log.Write(DEBUG, ost.str());
-
-		ost.str("");
-		ost	<< "CStatistics::~CStatistics: chrono duration = " << to_string(endChrono.count() - startChrono.count()) << " microseconds";
-		log.Write(DEBUG, ost.str());
-	}
+	MESSAGE_DEBUG("", "", "time duration (in ticks) = " + to_string((float)(endClock - startClock) /  CLOCKS_PER_SEC));
+	MESSAGE_DEBUG("", "", "sec duration = " + to_string(endTime - startTime));
+	MESSAGE_DEBUG("", "", "chrono duration = " + to_string(endChrono.count() - startChrono.count()) + " microseconds");
 }
